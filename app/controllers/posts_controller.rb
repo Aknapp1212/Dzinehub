d03c19f4d06d4881eb0333daf2de5e73e1e900fb
Adrian Knapp <Aknapp1212@gmail.com>
2016-01-24 16:40:54 -0500

    fixed editing ability on post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
   def index
+    @posts = Post.all.order("created_at DESC")
   end
 
   def show
@@ -22,12 +23,21 @@ class PostsController < ApplicationController
   end
 
   def edit
+
   end
 
   def update
+     if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
+
   def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
